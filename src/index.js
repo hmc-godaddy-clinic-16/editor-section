dcd330265fba8e3d3ba2b241c4c2428e97a082d2
LazyConi <wangkangn@hotmail.com>
2016-10-14 03:46:49 -0700

    display text, input box and button
    TODO: record the input data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Hello from "./hello.js";
+import InputBox from "./inputbox.js";
+import Button from "./button.js";
 
-ReactDOM.render(<Hello name={["HMU", "GoDaddy"]} special={true}/>, document.getElementById("app"));
+class Display extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>Title</p>
+				<InputBox />
+				<p>Text</p>
+				<InputBox />
+				<p><Button /></p>
+			</div>);
+	}
+}
+
+// TODO: store input when submit button is clicked.
+
+ReactDOM.render(<Display />, document.getElementById("app"));
---
+++ src/inputbox.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class InputBox extends React.Component {
+  constructor() {
+    super();    
+    this.state = {value: ""};
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  render() {
+    return (
+      <input
+        type="text"
+        value={this.state.value}
+        onChange={this.handleChange}
+      />
+    );
+  }
+}
+
+export default InputBox;
