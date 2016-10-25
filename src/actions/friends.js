55f27856f0440e72cc7566a5fdc1748f1bb41189
LazyConi <wangkangn@hotmail.com>
2016-10-25 07:52:33 -0700

    Revert "merge"
    This reverts commit 3275b49d268ac5a82f0442244e5f220ea326b56c.

--- src/actions/friends.js
+++
@@ -1,8 +0,0 @@
-import {ADD_FRIEND} from '../constants';
-
-export function addFriend (friend) {
-  return {
-    type: ADD_FRIEND,
-    friend
-  }
-}
--- src/reducers/friends.js
+++
@@ -1,9 +0,0 @@
-import {ADD_FRIEND} from '../constants';
-
-export default (state = [], action) => {
-  switch (action.type) {
-    case ADD_FRIEND:
-      return [...state, action.friend];
-  }
-  return state;
-}
