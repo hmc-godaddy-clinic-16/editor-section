59dd0db8c2f94bc79801c1b57b8355bcaee36ade
lbnelson <lbnelson@hmc.edu>
2017-02-11 17:18:25 -0800

    Mock site is done

--- src/components/mocksite.js
+++ src/components/mocksite.js
@@ -13,10 +13,39 @@ class MockSite extends React.Component {
 			<div className="mocksite">
 				<div className="mockheader"> 
 					<div className="mocknav">
+						<div className="col-1 mocknavtitle">Mr. Dough's Donuts</div>
 						<div className="col-1">Home&nbsp;&nbsp;&nbsp;&nbsp;Shop&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;Cart</div>
 					</div>
-					<h1 className="mocktitle">MR. DOUGH'S MARVELOUS DOUGHNUTS</h1>
+					<h1 className="mocktitle">MR. DOUGH'S DOUGHNUTS</h1>
 					<h4 className="mockphone">1-800-DOU-NUTS</h4>
+				</div>
+
+				<div className="mockabout"> 
+					<h3>ABOUT US</h3>
+					<hr/>
+					<p>Mr. Dough's doughnuts is a local doughnut establishment run by Mr. Dough and his friendly staff. We have the most delicious donuts around and a huge selection of flavors to select from. We're always happy to take large orders for events of all varieities. </p>
+					<h4>Flavors</h4>
+					<hr/>
+					<p> Our selection of donuts includes, but is not limited to, Lemon Cream, Apple Fritter, Cinnamon, Sugar, Custard, Vanilla Sprinkles, Double Chocolate, Powdered, Maple, Strawberry Jelly, Chocolate Sprinkles, Chocolate Long John, Boston Cream, Chocolate Glazed, and Glazed! </p>
+
+				</div>
+				<div className="mocklocation">
+					<h3>CONTACT US</h3>
+					<hr/>
+					<div>
+						<h4>Our Location</h4>
+						<p>
+							340 Donut Blvd
+							Donutland, CA 
+						</p>
+					</div>
+					<div className="col-6> mockhours">
+						<h4>Our Hours</h4>
+						<p>
+							Mon-Fri
+							8 am - 5 pm
+						</p>
+					</div>
 				</div>
 			</div>
 		);
