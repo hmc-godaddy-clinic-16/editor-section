88ddb65637e8f6f9eb9f9a3b0d4898644146fab2
LazyConi <wangkangn@hotmail.com>
2017-02-14 22:38:37 -0800

    added passport

---
+++ src/components/facebook.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import passport from 'passport';
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+class Facebook extends React.Component {
+
+	constructor(props) {
+	    super(props); 
+  	}
+
+	passport.use(new FacebookStrategy({
+	    clientID: 1056599564445365,
+	    clientSecret: e5ffee3067470ff41a1b133e0a20c8ce,
+	    callbackURL: "http://0.0.0.0:8080/webpack-dev-server/"
+	  },
+	  function(accessToken, refreshToken, profile, done) {
+	    User.findOrCreate(..., function(err, user) {
+	      if (err) { return done(err); }
+	      done(null, user);
+	    });
+	  }
+	));
+}
+
+export default Facebook; 
--- src/constants.js
+++ src/constants.js
@@ -9,8 +9,13 @@ export const UPDATE_LINK = 'UPDATE_LINK';
 // Client/Server communication
 // If a different host for the server is being used, change SERVER_URL
 // to the appopriate value so requests will be sent to the right place
-export const DEFAULT_ID = '583114642a35736e4aacc3ef';
-export const SERVER_URL = 'http://localhost:3000';
+
+// export const DEFAULT_ID = '583114642a35736e4aacc3ef';
+// export const SERVER_URL = 'http://134.173.43.36:3000';
+
+// local MongoDB host
+export const DEFAULT_ID = '588b8d11c835d451aca04872';
+export const SERVER_URL = 'http://localhost:3000'; // local host
+
 export const REQUEST_ANNOUNCEMENT = 'REQUEST_ANNOUNCEMENT';
 export const RECEIVE_ANNOUNCEMENT = 'RECEIVE_ANNOUNCEMENT';
-
