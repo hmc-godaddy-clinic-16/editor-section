24e8134050b2f06e017fbd10010fcb3e7115f3e1
lbnelson <lbnelson@hmc.edu>
2017-02-21 18:24:24 -0800

    Trying to get twitter session to work

--- server/FBserver.js
+++ server/FBserver.js
@@ -6,16 +6,77 @@ var TwitterStrategy = require('passport-twitter').Strategy;
 var mongoose = require('mongoose');
 
 // Connect to the database
-mongoose.connect('mongodb://localhost/passport');
+mongoose.connect('mongodb://localhost/announcementdb');
+console.log("Connected to the database");
+var db = mongoose.connection;
+console.log(db);
 
 // Create a user model
-var User = mongoose.model('User', {
-  platform: String,
-  oauthID: Number,
-  name: String,
-  created: Date,
-  token: String,
-  sharing: String
+var User = mongoose.model('User', 
+  new mongoose.Schema({
+    platform: String,
+    oauthID: Number,
+    name: String,
+    created: Date,
+    token: String,
+    sharing: String
+  })
+  );
+
+
+
+// Create a new Express application.
+var app = express();
+
+// Configure view engine to render EJS templates.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+//app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+// Headers
+app.use(function (req, res, next) {
+
+    // Allow these sites to make requests
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true, cookie: {secure: false} }));
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Facebook profile is serialized
+// and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(obj, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
 });
 
 
@@ -44,94 +105,45 @@ passport.use(new FacebookStrategy({
 passport.use(new TwitterStrategy( {
     consumerKey: config.twitter.consumerKey,
     consumerSecret: config.twitter.consumerSecret,
-    callbackURL: 'http://0.0.0.0:8080/'
+    callbackURL: 'http://localhost:4000/login/twitter/callback'
   }, 
- function(accessToken, refreshToken, profile, done) {
+ function(token, tokenSecret, profile, done) {
+    console.log("Got to the twitter strategy result");
     // If a twitter user doesn't exist in our database, create one
-    User.findOne({ platform: 'twitter' }, function(err, user) {
-      if(err) {
-        console.log(err);  // handle errors!
-      }
-      if (!err && user !== null) {
-        done(null, user);
-      } else {
-        user = new User({
-          platform: 'twitter',
-          oauthID: profile.id,
-          name: profile.displayName,
-          created: Date.now(),
-          token: accessToken,
-          sharing: 'true'
-        });
-        user.save(function(err) {
-          if(err) {
-            console.log(err);  // handle errors!
-          } else {
-            console.log("saving user ...");
-            done(null, user);
-          }
-        });
-      }
+    
+    process.nextTick(function() {
+      User.findOne({ platform: 'twitter' }, function(err, user) {
+        if(err) {
+          console.log(err);  // handle errors!
+        }
+        if (!err && user !== null) {
+          done(null, user);
+        } else {
+          user = new User({
+            platform: 'twitter',
+            oauthID: profile.id,
+            name: profile.displayName,
+            created: Date.now(),
+            token: accessToken,
+            sharing: 'true'
+          });
+          user.save(function(err) {
+            if(err) {
+              console.log(err);  // handle errors!
+            } else {
+              console.log("saving user ...");
+              done(null, user);
+            }
+          });
+        }
+      });
+
+    return done(null, profile);
     });
-    return cb(null, profile);
   }
 ));
 
 
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Facebook profile is serialized
-// and deserialized.
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(obj, done) {
-    User.findById(id, function (err, user) {
-      done(err, user);
-    });
-});
-
-
-// Create a new Express application.
-var app = express();
-
-// Configure view engine to render EJS templates.
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-
-// Headers
-app.use(function (req, res, next) {
-
-    // Allow these sites to make requests
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Pass to next layer of middleware
-    next();
-});
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-app.use(passport.initialize());
-app.use(passport.session());
 
 
 // Define routes.
@@ -194,7 +206,7 @@ var postToTwitter = function(req, res) {
 var getTwitterUser = function(req, res) {
   User.findOne({platform: 'twitter'}, function(err, user) {
     if (err || user == null) {
-      res.send({'error':'unable to get user'});
+      res.send({'error':'unable to get user' + err});
     } else {
       // We don't want to send the token back over an
       // insecure connection
@@ -206,7 +218,12 @@ var getTwitterUser = function(req, res) {
 
 
 /* Twitter API for our server*/
+
+// Authenticate
 app.get('/login/twitter', passport.authenticate('twitter'));
+app.get('/login/twitter/callback', passport.authenticate('twitter', {successRedirect: '/',
+  failureRedirect: '/'
+}));
 
 // Post 
 app.get('/twitter/post', postToTwitter);
